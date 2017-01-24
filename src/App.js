b5190594ce4ed668117e4b0cd48d475c37022ffb
Ian Hall <halli2008@yahoo.com>
2017-01-23 19:32:38 -0800

    adding login with firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 import { Header } from './components/common';
@@ -22,8 +23,10 @@ class App extends Component {
 
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View>
           <Header headerText="Manager" />
           <LoginForm />
